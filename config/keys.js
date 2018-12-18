21a62cfea3846e91cb9e092a2308ad565307b59a
jhubisz <jakub.hubisz@gmail.com>
2018-12-18 11:49:08 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
   googleClientID:
-    "754159758338-jeis1lo3cveuqo7d3hebmujuto8fr18p.apps.googleusercontent.com",
-  googleClientSecret: "PO3cNfiV7q1IFaTHXDiPOQSQ",
+    "",
+  googleClientSecret: "",
 
   mongoURI:
-    "mongodb://emaily-dev-user:server1907@ds227654.mlab.com:27654/emaily-dev",
+    "",
 
-  cookieKey: "kajshdkjlfhskjlfhsdf"
+  cookieKey: ""
 };
