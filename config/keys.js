065219370311398da80783ce639c816523befd9d
Kuba <jakub.hubisz@gmail.com>
2018-12-18 11:50:12 +0100

    config changes

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,5 @@
-module.exports = {
-  googleClientID:
-    "754159758338-jeis1lo3cveuqo7d3hebmujuto8fr18p.apps.googleusercontent.com",
-  googleClientSecret: "PO3cNfiV7q1IFaTHXDiPOQSQ",
-
-  mongoURI:
-    "mongodb://emaily-dev-user:server1907@ds227654.mlab.com:27654/emaily-dev",
-
-  cookieKey: "kajshdkjlfhskjlfhsdf"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
